e0feb1b33c003b303144e137fd34b736f41a03a3
Harish <harishpoctor@gmail.com>
2023-08-17 12:56:42 +0530

    Update build.sh - Making superuser creation work conditionally

--- build.sh
+++ build.sh
@@ -8,4 +8,11 @@ pipenv install
 
 pipenv run python manage.py collectstatic --no-input
 pipenv run python manage.py migrate
-pipenv run python manage.py createsuperuser --noinput
+
+# Check if a superuser already exists
+SUPERUSER_EXISTS=$(pipenv run python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).exists())")
+
+# Create superuser only if it doesn't exist
+if [ "$SUPERUSER_EXISTS" = "False" ]; then
+    pipenv run python manage.py createsuperuser --noinput
+fi
